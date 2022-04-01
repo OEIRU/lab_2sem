b290afd4a442de6ae2ae6c5441f17823d1a6809a
OEIRU <SpasiboMiryZaZemfiry@gmail.com>
2022-04-01 16:22:05 +0700

    ver 0.6

--- LAB1/source1.cpp
+++ LAB1/source1.cpp
@@ -1,57 +1,80 @@
-#include <stdio.h>
-#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
  
-int main() {
-    struct list {
-        list* next;
-        float elem;
-        int ch;
-    }
-    list * ph; * num; // ЭТО ВСЕГО ЛИШЬ СПИСКИ
-    list* p, * p2;
-    // p - первый полином
-    // p2 - второй полином
-    // p3 - сумма полиномов 
-    // counter - степень полинома
+double number;
+struct elems { double data; elems *next; } *S;
+void Make_elems(elems *S) // функция формирования списка
+{
+    elems *t;
+    t = S;
+    t2; \\ 
+    t3; \\ 
+    counter; \\ 
  
-    char ch;
-    int i = 0;
-    FILE* f;
-    fopen_s(&f, "input.txt", "r");
+    ifstream F;
+    F.open("file1.txt");
  
-    {ph = new list; // объявление ph главным звеном
-    fscanf_s(f, "%c", &ch); 
-    ph->next = NULL; // next - прямая ссылка
-    num->next = NULL;
-    p = ph; // сделать p заглавным звеном (заготовка для продвижения)
+    if (!F.is_open())
+    {
+        cout << "Ошибка откытия файла" << endl;
+    }
+    else
+    {
+        cout << "Файл открыт" << endl;
+        for // посчитать 2 раза
+        while (ch != '\n')
+        {
+            F >> number;
+            t->next = new elems;
+            t = t->next;                
+            t->data = number;           
+        }
+        
+        t->next = new elems;
+        t = t->next;
+        t->data = number;
+        t->next = NULL;
+        
+    }
+    F.close();
  
-    for (int i = 0; i < 2; i++){
-    while ((ch != '\n') {
-        p[i]->next = new list; // Создание очеревдного звена (продвижение)
-        p[i] = p[i]->next; // p присваиваем ссылку на очередное звено (действие на ячейку)
-        p[i]->elem = ch; // elem присваиваем значение символа ch (действие на содержание ячейки)
-        scanf(f, "%c", &ch);
-    }
-    p[i] = p[i]->next;
-    p[i] = NULL;
-    }
+}
  
-    list  *ph2;
-    for (p = ph, p2 = ph2; p != NULL, p2 != NULL; p = p->next, p2 = p2->next) {
-        p3 -> (p + p2); // бред какой-то 
-        p3 = p3->next;
-        // вывод в формате 
-        // p1 = 2x^3 + 3x^2 + 2x + 6
-        // p2 = 4x^3 + 6x^2 + 7x - 3
-        // p3 = 6x^3 + 9x^2 + 9x + 3
+void Count(elems *S, double number)
+{
+    elems *t;
+    for (t1 = S, t2 = S2; t1->next->next != NULL, t1->next->next != NULL; t1 = t1->next, t2->next)
+    {
+        t1->next->data -= number;
+        t3 = t1 + t2; 
+        t3 = t3 -> next; 
+        
     }
-    p3 -> next = NULL;
+    if (t1->next->next == NULL, t2->next->next == NULL)
+    { t->next = NULL; }
+}
  
-    // открыть файл для третьего
-    for (p3 = ph; p->next != NULL; p3 = p->next) {
-        // вывод значения p3 + значение степени через цикл for 
-        // (хотя n нужно как-то достать)
+void Print(elems* S)
+{
+    ofstream fout("file1.txt", ios_base::app);
+    elems *t;
+    fout << endl;
+    for (t3 = S->next; t3->next != NULL; t = t->next)
+    {
+        fout << t->data << " ";
+        // обозначить for для красивого "x^n"
     }
-    }
-    return 0;
-}   
+}
+ 
+int main() 
+{
+    setlocale(LC_ALL, "RUS");
+    S1 = new elems;
+    Make_elems(S1, S2);
+    Count(S, number);
+    Print(S);
+}
+ 
+// паста с угрозами
