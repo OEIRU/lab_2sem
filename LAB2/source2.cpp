bdf2d85530c50f4c29734464ba144ff7697dccb1
OEIRU <SpasiboMiryZaZemfiry@gmail.com>
2022-04-01 16:35:46 +0700

    ver 1.7 Прошу, убей меня.

--- LAB2/source2.cpp
+++ LAB2/source2.cpp
@@ -2,101 +2,105 @@
 #include <fstream>
 #include <string>
 using namespace std;
+int number;
+struct list3 {int el; list3  *next;} *S;
+struct dline { list3  *beg, *end ; } *M;
+ 
+ 
+void Read(list3* S) // функция формирования списка
+{
+    list3* t;
+    t = S;
+    
+    dline* l;
+    l = M;
+ 
     ifstream F;
-    int number;
-struct Queue { int x; Queue *Tail, *Head, *Next;}; // объявление структуры
-void Push(int x, Queue *&MyQueue){  // добавление элемента в очередь
-    Queue *element = new Queue; 
-    element->x = x; 
-    element->Tail = NULL;
-    if (MyQueue->Head == NULL)
-        MyQueue->Head = MyQueue->Next = element;
-    else{
-        (MyQueue->Next)->Tail = element;
-        MyQueue->Next = element;
-    }
-}
-void Show(Queue *MyQueue){ // функция вывода списка в файл 
+    F.open("input.txt");
  
-    ofstream fout ("output.txt", ios_base::app);
-    Queue *temp = MyQueue->Head; 
-    while (temp != NULL){ 
-        fout << temp->x << " "; 
-        temp = temp->Tail; 
+    if (!F.is_open())
+    {
+        cout << "Ошибка откытия файла" << endl;
     }
-        fout << endl; 
-}
-void ClearQueue(Queue *MyQueue){ // функция удаления из памяти
-    while (MyQueue->Head != NULL){ 
+    else {
+        cout << "Файл открыт" << endl;
  
-        Queue *temp = MyQueue->Head->Tail;
-        delete MyQueue->Head; 
-        MyQueue->Head = temp; 
-    }
-}
-void Pop(Queue *MyQueue){ // функция удаления элемента  
-    if (MyQueue->Head != NULL){ 
-        Queue *temp = MyQueue->Head->Tail; 
-        MyQueue->Head = temp; 
-    }
+        while (!F.eof())
+        {
+            F >> number; // заглавное звено
+            l->beg; 
+            // обозначаем и сдвигаем beg (да ничерта он не начальный)
+            t->next = new list3; // оно смеется надо мной 
+            t = t->next; 
+            t->el = number;
+        }
  
-}
-void CreateQueue(Queue *MyQueue){ // ввод из файла
-    ifstream fin("input.txt");
-    Queue *element = new Queue;
-    while (!fin.eof()){
-        fin >> number;
-        Push(number, MyQueue); 
-    } 
-}
-void DeleteNegative(Queue *MyQueue){ // удаление определенных элементов
-    Queue *temp = MyQueue->Head; 
-    while (temp != NULL){
-        temp = temp->Tail;
+        t->next = new list3;
+        t = t->next;
+        l->end; // обозначаем позицию end (А КУДА)
+        t->el = number;
+        t->next = NULL;
     }
-    temp = MyQueue->Head;
-    Pop(MyQueue);
+    F.close();
 }
-void AddNegative (int i, Queue *MyQueue) { // добавление определенных элементов
-    int count=0;
-    Queue *temp = MyQueue->Head; 
-    while (temp != NULL){
-        count++;
-        temp = temp->Tail;
-    }
-    temp = MyQueue->Head;
-    Push(i, MyQueue);
-    temp = temp->Tail;
-}
-void Checkman(Queue *MyQueue){ // проверка на пустоту
-    int count=0;
-    Queue *temp = MyQueue->Head;
-    while (temp != NULL){
-        count++;
-        temp = temp->Tail;
-    }
-    temp = MyQueue->Head;
  
-    for (int j = 0; j < count; j++){
-    if (temp->x = 0) {continue;}
-    else {cout << "Не пуст!" << endl;break;}
+ 
+int pushdline (dline *t, int el) // пушим 
+// t - херь, которая пробегает по списку
+// beg - начальный элемент
+// end - конечная
+{list3 *r;
+  r = new  list3;  r->el = el;  r->next = NULL; // он по новой это делаеТ?? Плевать, список уже создан. 
+  if (t->beg == NULL)  t->end = t->beg = r;       // добавление в пустую очередь
+  //Segmentation fault
+     else { t->end->next = r; t->end = r;}
+} 
+ 
+int  popdline (dline *t, int *el) // удоляем
+{ list3 *r;
+   if (t->beg == NULL)  
+        {return 0; cout << "Чел, очередь пуста" << endl;}         //очередь пуста
+  *el = t->beg-> el;  r = t->beg;
+  t->beg = t->beg->next;
+     delete r; 
+return 1;
+} 
+ 
+ void Print(list3* S)
+{
+    ofstream fout("output.txt", ios_base::app);
+    list3* t;
+    for (t = S->next; t->next != NULL; t = t->next)
+    {
+        fout << t->el << " "; 
     }
+    fout << endl;
 }
-int main(){
-    Queue *MyQueue = new Queue; 
-    CreateQueue(MyQueue); 
-    cout << "Проверили на пустоту" << endl;
-    Checkman(MyQueue);
-    cout << "Создали список" << endl;
-    Show(MyQueue); 
-    DeleteNegative(MyQueue);
-    cout << "Удалили элемент" << endl;  
-    Show(MyQueue);
-    AddNegative(42, MyQueue);
-    cout << "Добавили элемент" << endl;
-    Show(MyQueue);
-    ClearQueue(MyQueue);
-    delete MyQueue->Head;
-    delete MyQueue;
-    cout << "Память очищена" << endl;
-}
+ 
+ int main() {
+    S = new list3;
+    Read(S);
+    Print(S);
+    pushdline (M, 42);
+    popdline (M, 0);
+    Print(S);
+ 
+ }
+ 
+ /*
+Основная проблема - не читается оператор t, то есть я не понимаю, что в него вкладывать. 
+По сути он должен прогонять весь созданный список и смотреть в нем указатели beg и end. 
+НО ОН ВЫДАЕТ ПЕРЕПОЛНЕНИЕ СПИСКА, видимо не захватывает эти указатели. 
+Функции взял из методички. В них самих проблем нет. 
+Функцию чтения//ввода взял из прошлой лабы. 
+Марк, хелпани. 
+Надо как-то после создания объявить расположение этих указателей, чтобы потом на них сослаться. 
+ */
+ 
+/*
+Вторая часть задания - это создание деки
+Следовательно создание деки решит проблему буквы А, и сделает код более гибким. 
+Нужно лишь поменять направление движения динамического списка. 
+ 
+Написания нужной нам деки не нашел, придется брать пример из лекции 1_2. 
+*/ 
