68a688cfa71616f50b65b53d2daec3d8b06c7c52
OEIRU <SpasiboMiryZaZemfiry@gmail.com>
2022-04-01 16:36:53 +0700

    ver 1.9 Linux rules

--- LAB2/source2.cpp
+++ LAB2/source2.cpp
@@ -1,124 +1,113 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
-int number;
-struct list3
-{ char el; list3 *next; } *S; // ГРЕБАННЫЙ СПИСОК
-struct dline
-{ list3 *beg, *end ; } *t; // ПУТЕВОДИТЕЛЬ ПО СПИСКУ
+#include <stdio.h>
  
-void Read(list3* S  ) // функция формирования списка
+struct list
 {
-    list3* t;
-    t = S;
+    int el;
+    list *next;
+};
  
-    ifstream F;
-    F.open("input.txt");
- 
-    if (!F.is_open())
-    {
-        cout << "Файл не открылся! Проверь название/существование" << endl;
-    }
-    else {
-        cout << "Opening file" << endl;
- 
-        while (!F.eof())
-        {
-            F >> number; // заглавное звено
-            t->next = new list3; // оно смеется надо мной 
-            t = t->next; // нееет
-            t->el = number; // осмысленные комментарии
-        }
- 
-        t->next = new list3; // край мира
-        t = t->next;
-        t->el = number;
-        t->next = NULL;
-        cout << "Я прочитал твой файл, хозяин!" << endl;
-    }
-    F.close(); cout << "Я закрыл файл, с которого читал"<< endl;
-}
-// список создан, катаемся по dline
- 
-int pushdline (dline* t, int el) // пушим
-{list3 *r; // пробегает список походу
-  r = new  list3;  r->el = el;  r->next = NULL; // он по новой это делаеТ?? П-плевать, давай, читай откуда-то, мы же миллионеры 
-  if (t->beg == NULL)  {t->end = t->beg = r; cout << "Добавился в пустую очередь" << endl;}  // добавление в пустую очередь
-  //Segmentation fault, этого указателя t! (переехал на 56 строку)
-     else { t->end->next = r; t->end = r; cout << "Ну я добавил в самое начало, оке?" << endl;}
-} 
- 
-int  popdline (dline* t, int *el) // удоляем
-{ list3 *r;
-   if (t->beg == NULL)  
-        {return 0; cout << "Чел, очередь пуста" << endl;}
-        // ДА КТО ТАКОЙ *el... указатель на el (порядок el? или положение. Точно, положение в списке)
-  *el = t->beg-> el;  r = t->beg; // захватываем элемент
-  // убивать, убивать, убивать, убивать.
-  t->beg = t->beg->next; // шо я делаю? 
-     delete r; 
-     cout << "Удалил" << endl;// удалять, удалять, удалять
-return 1; // оффнуть программу (может не надо?)
-} 
- 
- 
- // не кастуется
- void Print(list3 *S)
+struct queue
 {
-    ofstream fout("output.txt", ios_base::app);
-    list3* t;
-    for (t = S->next; t->next != NULL; t = t->next)
-    {
-        fout << t->el << " "; 
-    }
-    fout << endl;
+    list *beg, *end;
+};
+ 
+void putToQueue(queue *q, int iEl)   //добавить в очередь
+{
+    list *tmp;
+    tmp = new list;
+    tmp->next = NULL;
+    tmp->el = iEl;
+    if(q->end != NULL)
+        q->end->next = tmp;
+    else
+        q->beg = tmp;
+    q->end = tmp;
 }
  
- // ДА НЕ БЫЛО НИКАКОГО *S
- // НИКОГО ТЫ НЕ СБИВАЛ 510 ЛЕТ НАЗАД
- // *S НИКОГДА НЕ СУЩЕСТВОВАЛО
- // ОН ВСЕГО ЛИШЬ УКАЗАТЕЛЬ
+int takeFromQueue(queue *q, int *iEl) // взять из очереди
+{
+    if(q->beg == NULL) return 0;
+    list *tmp;
+    tmp = q->beg;
+    *iEl = tmp->el;
+    q->beg = tmp->next;
+    delete tmp;
+    if(q->beg == NULL) q->end = NULL;
+    return 1;
+}
  
+queue *CreateQueue()            //создать очередь
+{
+    queue *q;
+    q = new queue;
+    q->beg = NULL;
+    q->end = NULL;
+    return q;
+}
  
- int main() {
-    S = new list3;
-    t = new dline; // Никакой связи с S
-    Read(S);
-    // на данном этапе у нас лежит полностью готовый список в list3
-    Print(r);
-    pushdline (t, 42);
-    pushdline (t, 42);
-    pushdline (t, 42);
+int isQueueEmpty(queue *q)      //проверка очереди на пустоту
+{
+    if(q->beg == NULL) return 1;
+    return 0;
+}
  
-    popdline (t, 0); // вместо нуля должна быть ссылка? 
-    Print(S);
- }
+int ClearQueue(queue *q)        //очистка очереди
+{
+    if(q->beg == NULL) return 0;
+    list *tmp, *t;
+    tmp = q->beg;
+    while(tmp->next != NULL)
+    {
+        t = tmp;
+        tmp = t->next;
+        delete t;
+    }
+    q->beg = NULL;
+    q->end = NULL;
+    return 1;
+}
  
- // Я МЫСЛЮ, СЛЕДОВАТЕЛЬНО СУЩЕСТВУЮ, МАРК, НЕСИ КАПИТАЛ!
- // Ачивка "Полное погружение"
- // Пересмотреть "Сияние", держа в руках топор.
+void PrintQueue(queue *q)       //вывестии очередь на экран
+{
+    printf("\n");
+    queue *tmp = CreateQueue();
+    int iEl;
+    while(!isQueueEmpty(q))
+    {
+        takeFromQueue(q, &iEl);
+        printf("%d ", iEl);
+        putToQueue(tmp, iEl);
+    }
+    while(!isQueueEmpty(tmp))
+    {
+        takeFromQueue(tmp, &iEl);
+        putToQueue(q, iEl);
+    }
+}
+int main()
+{
+    int i;
+    queue *q = CreateQueue();
+    printf(".......Put Elems..............");
+    for(i=30; i<40; i++)
+    {
+        putToQueue(q, i);
+        PrintQueue(q);
+    }
+    printf("\n......Take Elems...............");
+    while(!isQueueEmpty(q))
+    {
+        takeFromQueue(q, &i);
+        PrintQueue(q);
+    }
+    printf("\n........Put Elems.............");
+    for(i=30; i<35; i++)
+    {
+        putToQueue(q, i);
+        PrintQueue(q);
+    }
+    printf("\n........Clear queue.............");
+    ClearQueue(q);
+    PrintQueue(q);
  
- /*
- Что же пишут в газетах в разделе «Из зала суда»?
-Приговор приведен в исполненье. Взглянувши сюда,
-обыватель узрит сквозь очки в оловянной оправе,
-как лежит человек вниз лицом у кирпичной стены;
-но не спит. Ибо брезговать кумполом сны
-продырявленным вправе.  
-*/
-// Есть такой зверек - вомбат, а готовой лабы так и нет!
-// Да кто такой ваш Бродске 
- 
-/*
-int main() {
-    S = NULL;
-    t = NULL;
-    pushdline (t, 0);
-    pushdline (t, 15);
-    pushdline (t, 13);
-    popdline(t, 0);
-    Print(S);
-}*/
- 
- 
+}
