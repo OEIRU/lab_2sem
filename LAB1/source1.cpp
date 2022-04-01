8825e094e960c1f4bd4e0f5b89658b433151fc65
OEIRU <SpasiboMiryZaZemfiry@gmail.com>
2022-04-01 16:16:25 +0700

    ver 0.3

--- LAB1/source1.cpp
+++ LAB1/source1.cpp
@@ -2,41 +2,33 @@
 #include <cmath>
  
 int main() {
-    // Работа с файлами - later
-    // ввод массива P
-    struct polinomP { char l; polinomP* next; };
-    polinomP* head, * p; char ch;
-    head = p = new polinomP;
-    scanf_s("%c", &(p->l));
-    scanf_s("%c", &ch);
+    struct polinom { char l; polinom* next; };
+    polinom* head, * p; polinom* head2, *p2; char ch;
+    fopen_s(&f, "input.txt", 'r');
+    head = p = new polinom;
+    fopen_s(f, &f, "input.txt", 'r');
+    fscanf_s(f, "%c", &ch);
+    int i;
     while (ch != '\n') {
-        p->next = new polinomP; p = p->next; p->l = ch;
-        scanf_s("%c", &ch);
+        p->next = new polinom; 
+        p = p->next; 
+        p->l = ch;
+        scanf_s(f, "%c", &ch);
     }
-    p->next = NULL; // ура, запустилось 
+    p2 = p;
+    p->next = NULL;
  
-// КАК ВЫВЕСТИ ПОЛИНОМ
-    // Пока не конец строки
-    // ввод массива Q
-    struct polinomQ { char l; polinomQ* next; };
-    polinomQ* head, * q; char ch;
-    head = q = new polinomQ;
-    scanf_s("%c", &(q->l));
+    head2 = p2 = new polinom;
+    scanf_s("%c", &(p2->l));
     scanf_s("%c", &ch);
     while (ch != '\n') {
-        q->next = new polinomQ; q = q->next; q->l = ch;
-        scanf_s("%c", &ch);
+        p2->next = new polinom;
+        p2 = p2->next;
+        p2->l = ch;
+        scanf_s(f, "%c", &ch);
     }
-    q->next = NULL; // ура, не запустилось
+    p2->next = NULL;
+ 
+    fclose(f);
+    return 0;
 }
-// Ну а тут чисто по дефолту
-//     рассмотрим размерности двух списков
-//     возьмем наибольшее
-//     и начнем поиндексально складывать
- 
-    // Первая редакция программы - два полинома делаются в 2 прогона
-    // Вторая редакция программы - два полинома делаются в 
-    // одном динамическом массиве
-    // Третья редакция программы - работа с файлами
-    // Четвертая редакция программы - автоматизация подсчета A[1][i] + A[2][i]
- 
