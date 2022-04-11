70d83cacf738bb602063f9cdf6a72896d3b3d6cb
Vladimir <spasibomiryzazemfiry@gmail.com>
2022-04-12 02:10:40 +0700

    Ночь перед сдачей 3 лабы

--- LAB3/source3.cpp
+++ LAB3/source3.cpp
@@ -1,71 +1,109 @@
-#include "stdio.h"
+#include <iostream>
+#include <fstream>
 #include <locale.h>
+using namespace std;
 
-struct btree
-{
-    int elem;
-    btree *left, *right;
+
+struct btree {        // Структура дерева, можете заметить, что изначально каждый чаровский элемент это %. это необбходимо для проверки
+	char elem;
+	float num; 
+	btree* left, * right;
 };
 
-struct stack
-{
-    btree *elem;
-    stack *next;
+struct stek {     // структура стека для дальнейшего прямого обхода
+	btree* tree; // туда
+	stek* back;  // обратно
 };
 
-//Ввод дерева
-btree *build_tree(FILE *A)
+btree* build_tree(fstream* doc)     // Функция считывания дерева из файла "input.txt" 
 {
-    char sym;
-    btree *d;
-    fscanf(A, "%c", &sym);
-    switch (sym)
-    {
-    case '(':
-    {
-        d = new btree;
-        fscanf(A, "%c", &sym);
-        d->elem = sym;
-        d->left = build_tree(A);
-        d->right = build_tree(A);
-        fscanf(A, "%c", &sym);
-        return d;
-    }
-    case '0':
-        return NULL;
-    case ',':
-        d = build_tree(A);
-        break;
-    }
+	char sym;
+	float num;
+	btree* d;
+	*doc >> sym;
+	switch (sym)
+	{
+	case '(':
+	{
+		d = new btree;
+		*doc >> sym;
+		d->elem = sym;
+		d->left = build_tree(doc);
+		d->right = build_tree(doc);
+		*doc >> sym;
+		return  d;
+	}
+	case '0':
+		return NULL;
+	case ',':
+	{
+		d = build_tree(doc);
+		break;
+	}
+	}
 }
 
-//Вывод дерева
-void print_btree(btree *d)
-{
-    if (d == NULL)
-        return;
-    if (d->left == NULL)
-    {
-        printf("%c", d->elem);
-        return;
-    }
-    print_btree(d->left);
-    printf("%c", d->elem);
-    print_btree(d->right);
+void prem_obhod(btree* head) {  // функция прямого обхода и вывода элементов дерева
+	
+	int n;
+	btree* d = head;
+	stek* sth = new stek, * stn = NULL;
+	sth->back = NULL;
+	sth->tree = NULL;
+
+
+	while (d != NULL)  // ГРЕБАННЫЙ ОБХОД В ГЛУБИНУ
+		// НАМ В ШИРИНУ ТО НУЖЕН
+		// МАРКО, ГДЕ ОТВЕТЫ НА ВОПРОСЫ ВСЕЛЕННОЙ
+		// И ВСЕГО МИРОЗДАНИЯ
+
+
+	{
+		cout << d->elem;
+		if ((d->left != NULL && d->right != NULL))
+		{
+			stn = new stek;
+			stn->back = sth;
+			sth = stn;
+			stn->tree = d->right;
+			// cout << stn << endl;
+			d = d->left;
+		}
+
+		else
+		{
+			if (d->left == NULL && d->right == NULL) // Если все же возникнет вопрос, то по большей части этот обход организован по тому принципу, который давался на лекции
+				if (sth->tree != NULL)
+				{
+					d = stn->tree;
+					sth = stn->back;
+					delete stn;
+					stn = sth;
+				}
+				else
+					d = NULL;	
+			else
+				if (d->left != NULL)
+					d = d->left;
+				else
+					d = d->right;
+		}
+	}
 }
 
-    btree *build_tree(FILE *A);
 
-    
-void main()
-{
-	btree *d;
-	FILE *A;
-	fopen(&A, "input.txt");
-	d = build_tree(A);
-	fclose(A);
-	print_btree(d);
-    // тут нужен посимвольный ввод
-    // взять из прошлой лабы
-    // ходим в поисках смысла до определенного уровня.
+int main() {                 // основная функция т.к. все фукции описаны выше, мы просто вызываем их исполнение
+	setlocale(LC_CTYPE, "Russian");
+	fstream in("input.txt");
+	btree* root = build_tree(&in);
+		prem_obhod(root);
+		cout << " - Прямой обход" << endl;
+	return 1;
 }
+
+// и я почти не сплю
+// да кому это сдалось! 
+// СНА НЕ СУЩЕСТВУЕТ
+// страдать страдать страдать
+// так и не написал письмо
+
