8311d2d4757e0749347a4b7a6cb51703d9865e7a
Vladimir <spasibomiryzazemfiry@gmail.com>
2022-06-01 10:46:23 +0700

    Reliz 58

--- LAB5/Source5.cpp
+++ LAB5/Source5.cpp
@@ -77,7 +77,20 @@ void add(int team, table* T, int jup) {
 	}
 }
 
-
+void QuickSort(int l, int r, table* a) {
+	int x, i, j;
+	if (l < r) {
+		x = a[(l + r) / 2].first;
+		i = l; j = r;
+		while (i <= j) {
+			while (a[i].first < x) i++;
+			while (a[j].first > x) j--;
+			if (i <= j) { swap(a[i++].first, a[j--].first); swap(a[i++].first, a[j--].first); }
+		}
+		QuickSort(l, j, a);
+		QuickSort(i, r, a);
+	}
+}
 
 int main() {
 	setlocale(LC_CTYPE, "Russian");
@@ -98,56 +111,53 @@ int main() {
 	add(team, T, jup);
 
 	int temp, temp1, temp2, temp3;
-	
+
 	for (int i = 0; i < N - 1; i++) { // лютый костыль по сортировке ()
 
 		for (int j = 0; j < N - i - 1; j++) // работает, не трожь 
 			if (T[j].index < T[j + 1].index) { // НУЛИ В КОНЦЕ
 
 					// меняем элементы местами
-					temp = T[j].index;
-					T[j].index = T[j + 1].index;
-					T[j + 1].index = temp;
-					// меняем элементы first
-					temp1 = T[j].first;
-					T[j].first = T[j + 1].first;
-					T[j + 1].first = temp1;
-					// меняем элементы last
-					temp2 = T[j].last;
-					T[j].last = T[j + 1].last;
-					T[j + 1].last = temp2;
-					// меняем элементы three 
-					temp3 = T[j].three;
-					T[j].three = T[j + 1].three;
-					T[j + 1].three = temp3;
-					cout << T[i].index << "  " << T[i].first << "  " << T[i].last << "  " << T[i].three << endl;
+				temp = T[j].index;
+				T[j].index = T[j + 1].index;
+				T[j + 1].index = temp;
+				// меняем элементы first
+				temp1 = T[j].first;
+				T[j].first = T[j + 1].first;
+				T[j + 1].first = temp1;
+				// меняем элементы last
+				temp2 = T[j].last;
+				T[j].last = T[j + 1].last;
+				T[j + 1].last = temp2;
+				// меняем элементы three 
+				temp3 = T[j].three;
+				T[j].three = T[j + 1].three;
+				T[j + 1].three = temp3;
+				cout << T[i].index << "  " << T[i].first << "  " << T[i].last << "  " << T[i].three << endl;
 
 			}
 
 		// bruh moment перестановок
 	}
-	cout << "https://youtu.be/dQw4w9WgXcQ" << endl;
+
+	int count = 0;
+	for (int i = 0; i < N; i++) {
+		if (T[i].first != 0 || T[i].last || 0 && T[i].three || 0)
+			count++;
+	}
 	for (int i = 0; i < N; i++)
-		cout << "  " << T[i].index << "  " << T[i].first << "  " << T[i].last << "  " << T[i].three << endl;
+		if (T[i].first != 0 || T[i].last || 0 && T[i].three || 0)
+			cout << "  " << T[i].index << "  " << T[i].first << "  " << T[i].last << "  " << T[i].three << endl;
+
+
 
 	/// 5 ЛАБА CONFERDED
-	cout << "5 ЛАБА, РАСПРОСТРАНИТЕ" << endl;
-	for (int i = 0; i < N - 1; i++) { // да мне уже все равно 
-		for (int j = 0; j < N - i - 1; j++) { // как выйдет
-			if (T[j].first < T[j + 1].first) {
-				// меняем index
-				temp = T[j].index;
-				T[j].index = T[j + 1].index;
-				T[j + 1].index = temp;
-				// меняем first
-				temp1 = T[j].first;
-				T[j].first = T[j + 1].first;
-				T[j + 1].first = temp1;
-			}
-		}
-	}
-	cout << "https://youtu.be/fpozpsc1AnQ" << endl;
-	for (int i = 0; i < N; i++)
+	cout << "Подсчет по проценту первых мест" << endl;
+	QuickSort(0, N - 1, T);
+
+	for (int i = 0; i < count; i++)
 		cout << "  " << T[i].index << "  " << T[i].first << "  " << T[i].last << "  " << T[i].three << endl;
+
 	return 0;
 }
+	
